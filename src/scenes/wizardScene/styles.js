6185d77c86bbf96d4c22382ac3d4bea7d30e7f87
Sergio <sera.serduk@gmail.com>
2018-04-03 14:17:45 +0300

    adding small fixes to wizardScene

--- src/scenes/wizardScene/styles.js
+++ src/scenes/wizardScene/styles.js
@@ -3,21 +3,9 @@ import {StyleSheet, Dimensions} from "react-native";
 var {height, width} = Dimensions.get('window');
 
 const module_style = StyleSheet.create({
-	welcomeImage:{
-		width: 250,
-		height: 250,
-		marginBottom: 30,
-		alignSelf:'center'
-	},
-	welcomeTitle:{
-		fontSize: 30,
-		fontWeight: 'bold',
-		textAlign: 'center',
-		marginBottom: 10
-	},
-	welcomeText:{
-		fontSize: 20,
-		textAlign: 'center'
+	image: {
+		width: 320,
+		height: 320,
 	}
 });
 
--- src/scenes/wizardScene/wizardScene.js
+++ src/scenes/wizardScene/wizardScene.js
@@ -1,13 +1,6 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
 import AppIntroSlider from 'react-native-app-intro-slider';
-
-const styles = StyleSheet.create({
-	image: {
-		width: 320,
-		height: 320,
-	}
-});
+import module_style from "../../scenes/wizardScene/styles";
 
 const slides = [
 	{
@@ -15,7 +8,7 @@ const slides = [
 		title: 'Title 1',
 		text: 'Description.\nSay something cool',
 		image: require('../../../assets/1.jpg'),
-		imageStyle: styles.image,
+		imageStyle: module_style.image,
 		backgroundColor: '#59b2ab',
 	},
 	{
@@ -23,15 +16,15 @@ const slides = [
 		title: 'Title 2',
 		text: 'Other cool stuff',
 		image: require('../../../assets/2.jpg'),
-		imageStyle: styles.image,
+		imageStyle: module_style.image,
 		backgroundColor: '#febe29',
 	},
 	{
 		key: 'somethun1',
 		title: 'Rocket guy',
-		text: 'I\'m already out of descriptions\n\nLorem ipsum bla bla bla',
+		text: 'I\'m already out of descriptions\nLorem ipsum bla bla bla111',
 		image: require('../../../assets/3.jpg'),
-		imageStyle: styles.image,
+		imageStyle: module_style.image,
 		backgroundColor: '#22bcb5',
 	}
 ];
