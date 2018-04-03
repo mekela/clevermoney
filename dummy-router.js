17655482e5c43031a436405776815eec74e0e536
Sergio <sera.serduk@gmail.com>
2018-04-03 10:15:38 +0300

    modifing some typo issues in file naming Expences -> Expenses

--- dummy-router.js
+++ dummy-router.js
@@ -3,9 +3,9 @@ import React from 'react';
  import App from './src/scenes/loginScene';
 // import App from './src/scenes/wizardScene';
 // import App from './src/scenes/homepageScene';
-// import App from './src/scenes/addExpancesScene';
-// import App from './src/scenes/categoryExpancesScene';
-// import App from './src/scenes/statisticExpencesScene';
+// import App from './src/scenes/addExpensesScene';
+// import App from './src/scenes/categoryExpensesScene';
+// import App from './src/scenes/statisticExpensesScene';
 // import App from './src/scenes/profileScene';
 
 export default App;
