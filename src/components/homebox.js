3f50e8e4519b1987d1aab96fda5dce4150d889c8
alex <sania_free@i.ua>
2018-04-11 20:43:23 +0300

    router fix

--- src/components/homebox.js
+++ src/components/homebox.js
@@ -1,10 +1,12 @@
 import { TouchableOpacity, Text, Image, View} from "react-native";
 import React from 'react';
 import styles from "../themes/styles";
+import {Actions} from "react-native-router-flux";
 
 
 const Homebox = (props)=>(
   <TouchableOpacity
+    onPress={props.press}
     style={ [styles.homeBox, props.additionalStyle] }
   >
     <View style= { styles.homeImageWrap } >
--- src/scenes/homepageScene/homepageScene.js
+++ src/scenes/homepageScene/homepageScene.js
@@ -15,10 +15,10 @@ class App extends Component{
 			<View style = { styles.container }>
 				<Header title="CLEVER MONEY" />
 				<View style = { style_module.containerHome }>
-					<Homebox  onPress={Actions.addExpensesScene}  text="Додати витрати" pic={{uri: 'https://facebook.github.io/react/logo-og.png'}}/>
-					<Homebox  onPress={Actions.statisticExpensesScene}  text="Статистика" pic={{uri: 'https://facebook.github.io/react/logo-og.png'}}/>
-					<Homebox  onPress={Actions.categoryExpensesScene}  text="Категорії" pic={{uri: 'https://facebook.github.io/react/logo-og.png'}}/>
-					<Homebox  onPress={Actions.profileScene}  text="Мій профіль" pic={{uri: 'https://facebook.github.io/react/logo-og.png'}}/>
+					<Homebox  press={Actions.addExpensesScene}  text="Додати витрати" pic={{uri: 'https://facebook.github.io/react/logo-og.png'}}/>
+					<Homebox  press={Actions.statisticExpensesScene}  text="Статистика" pic={{uri: 'https://facebook.github.io/react/logo-og.png'}}/>
+					<Homebox  press={Actions.categoryExpensesScene}  text="Категорії" pic={{uri: 'https://facebook.github.io/react/logo-og.png'}}/>
+					<Homebox  press={Actions.profileScene}   text="Мій профіль" pic={{uri: 'https://facebook.github.io/react/logo-og.png'}}/>
 				</View>
 				<Nav />
 			</View>
