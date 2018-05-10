04222129f4209bfc981a6c95bcdb094d035c5505
alex <sania_free@i.ua>
2018-05-10 10:35:39 +0300

    take photo

--- android/app/src/main/java/com/clevermoney/MainApplication.java
+++ android/app/src/main/java/com/clevermoney/MainApplication.java
@@ -3,12 +3,15 @@ package com.clevermoney;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.imagepicker.ImagePickerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.horcrux.svg.SvgPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.imagepicker.ImagePickerPackage; // <-- add this import
+
 
 import java.util.Arrays;
 import java.util.List;
@@ -25,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ImagePickerPackage(),
             new VectorIconsPackage(),
             new SvgPackage()
       );
--- src/scenes/loginScene/loginScene.js
+++ src/scenes/loginScene/loginScene.js
@@ -1,7 +1,8 @@
-import { View, Text, Image, ScrollView, ImageBackground, KeyboardAvoidingView, TouchableOpacity } from 'react-native';
+import { AppRegistry, PixelRatio, View, Text, Image, ScrollView, ImageBackground, KeyboardAvoidingView, TouchableOpacity } from 'react-native';
 import React, { Component } from 'react';
 import { Actions } from 'react-native-router-flux';
 import Icon from "react-native-vector-icons/EvilIcons";
+import ImagePicker from "react-native-image-picker";
 import {signIn} from "../../actions";
 
 import styles from "../../themes/styles";
@@ -11,6 +12,50 @@ import Loader from "../../components/loader";
 import style_module from "./styles";
 
 class App extends Component{
+
+	state = {
+
+		ImageSource: null,
+
+	};
+
+	selectPhotoTapped() {
+		const options = {
+			quality: 1.0,
+			maxWidth: 500,
+			maxHeight: 500,
+			storageOptions: {
+				skipBackup: true
+			}
+		};
+
+		ImagePicker.showImagePicker(options, (response) => {
+			console.log('Response = ', response);
+
+			if (response.didCancel) {
+				console.log('User cancelled photo picker');
+			}
+			else if (response.error) {
+				console.log('ImagePicker Error: ', response.error);
+			}
+			else if (response.customButton) {
+				console.log('User tapped custom button: ', response.customButton);
+			}
+			else {
+				let source = { uri: response.uri };
+
+				// You can also display the image using data:
+				// let source = { uri: 'data:image/jpeg;base64,' + response.data };
+
+				this.setState({
+
+					ImageSource: source
+
+				});
+			}
+		});
+	}
+
 	constructor() {
 		super();
 		this.state = {
@@ -59,8 +104,13 @@ class App extends Component{
 				<ScrollView style= { style_module.loginForm } >
 				<ImageBackground source={require('../../../assets/bgLogin.jpg')} style= { style_module.bgLogin } >
 					<View style= { style_module.loginImageWrap } >
-						<Image source={require('../../../assets/clover.png')}
-							   style= { style_module.loginImage }  />
+
+						<TouchableOpacity onPress={this.selectPhotoTapped.bind(this)}>
+
+							<Image source={this.state.ImageSource} style= { style_module.loginImage }  />
+							<Text>Select a Photo</Text>
+								 
+						</TouchableOpacity>
 					</View>
 				</ImageBackground>
 
